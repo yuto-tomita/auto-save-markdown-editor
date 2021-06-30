eda30bd55b5e5b635507e6c51cb0b10830545fdb
yuto-tomita <qualidea01@gmail.com>
2021-06-30 23:39:02 +0900

    eslintの設定を修正

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -12,19 +12,6 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"semi": "error"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+  }
 };
