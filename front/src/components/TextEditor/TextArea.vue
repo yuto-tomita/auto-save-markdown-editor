27e240b2bac6f9b541cb8caaabb0325c56982b27
yuto-tomita <qualidea01@gmail.com>
2021-07-08 21:25:28 +0900

    ファイル名を変更し、テキストエリアの簡単なスタリング

---
+++ front/src/components/TextEditor/TextArea.vue
@@ -0,0 +1,25 @@
+<template>
+  <textarea
+    v-model="inputText"
+    class="block border-solid border-2 outline-none w-full textarea-height"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const inputText = ref<string>('');
+    return {
+      inputText
+    };
+  }
+});
+</script>
+
+<style>
+.textarea-height {
+  height: 80vh;
+}
+</style>
