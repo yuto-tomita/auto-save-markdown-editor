1b23524a5baa4101dea42471c0a5306c959dab9a
yuto-tomita <qualidea01@gmail.com>
2021-07-10 22:48:09 +0900

    変更監視しているmarkdownEditorからHTML形式に変換するように加筆

--- front/src/components/TextEditor/TextArea.vue
+++ front/src/components/TextEditor/TextArea.vue
@@ -3,16 +3,27 @@
     v-model="markdownText"
     class="border border-gray-800 w-full textarea-height ml-1 focus:outline-blue-50 p-4"
   />
+  {{ html }}
+  <!-- {{ markdownText }} -->
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, watchEffect } from 'vue';
+import marked from 'marked';
 
 export default defineComponent({
   setup() {
     const markdownText = ref<string>('');
+    const html = ref<string>('');
+
+    /** 入力されたmarkdownテキストをHTML形式に変換させる */
+    watchEffect(() => {
+      html.value = marked(markdownText.value);
+    });
+
     return {
-      markdownText
+      markdownText,
+      html
     };
   }
 });
