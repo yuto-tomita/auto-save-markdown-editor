e8df23283b4734b2e1bdecee37c51296d2fe1095
yuto-tomita <qualidea01@gmail.com>
2021-07-13 21:16:47 +0900

    環境変数が読み込めていることを確認

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+func connectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("環境変数読み込みエラー")
+	}
+	fmt.Println(os.Getenv("DB_NAME"))
+}
+
 func main() {
+	connectDB()
 	db, err := sql.Open("mysql", "root:rootpassword@tcp(mysql-container:3306)/test-db")
 
 	if err != nil {
