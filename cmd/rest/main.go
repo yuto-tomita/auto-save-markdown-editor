d1f2bcb3941a5642b853acf873fe0a60627670e6
yuto-tomita <qualidea01@gmail.com>
2021-07-14 21:55:29 +0900

    DB接続を他ファイルに切り出し

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"os"
-
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
+	"github.com/yuto-tomita/auto-save-markdown-editor/cmd/pkg/db"
 )
 
-func connectDB() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("環境変数読み込みエラー")
-	}
-	
-	db, err := sql.Open(
-		os.Getenv("DB_CONNECTION"), os.Getenv("DB_NAME") + ":" + os.Getenv("DB_PASS") + "@tcp(mysql-container:3306)/" + os.Getenv("DB_NAME"))
-	if (err != nil) {
-		fmt.Println("DB接続エラー")
-	}
-
-	fmt.Println(db)
-}
 
 func main() {
-	connectDB()
+	db := db.Connect()
 }
