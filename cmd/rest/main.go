e380fe65a724e9ba432b78aad89362fd0297aca5
yuto-tomita <qualidea01@gmail.com>
2021-07-15 09:36:43 +0900

    nginxが正常に動作しているかの確認のため、ルーティングのためのパッケージを導入

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-	"myapp/pkg/db"
+	// "myapp/pkg/db"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello world!")
+}
 
 func main() {
-	db := db.ConnectDB()
+	// db := db.ConnectDB()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", rootHandler)
+
+	http.Handle("/", r)
+	http.ListenAndServe(":3000", nil)
 }
