a8d6fd23b55b70520b63c35cf36e9e1c4213cdef
yuto-tomita <qualidea01@gmail.com>
2021-07-16 09:40:08 +0900

    うまく行かなかったためmainにserveを記述

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	// "myapp/pkg/db"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello world!")
-}
-
 func main() {
-	// db := db.ConnectDB()
-
 	r := mux.NewRouter()
-	r.HandleFunc("/", rootHandler)
+	r.HandleFunc("/", index)
 
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello world")
 }
