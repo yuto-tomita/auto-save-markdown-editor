6935f9d6f74b0841632bdcd99eea29ac2898c352
yuto-tomita <qualidea01@gmail.com>
2021-07-24 19:33:01 +0900

    piniaを使用するための準備

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
+import { pinia } from './store';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(pinia);
+
+app.mount('#app');
