ccdf80550952b64a249591909129f8fcf2cd0887
yuto-tomita <qualidea01@gmail.com>
2021-07-24 19:33:13 +0900

    実際にpiniaにアクセスできるかどうか確認

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <h1 class="text-center">
     AutoSaveMarkdownEditor
+    {{ res }}
   </h1>
   <TextEditor @onChangeTextArea="handleEvent" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import TextEditor from '@/components/TextEditor/TextArea.vue';
+import { apiStore } from '@/store/api';
 
 export default defineComponent({
   components: {
     TextEditor
+  },
+  setup () {
+    console.log('test');
+    const test = apiStore();
+    const res = ref({});
+
+    getDraft();
+
+    async function getDraft () {
+      try {
+        await test.getDraftList();
+        res.value = test.returnCalendarList;
+        // console.log(test.returnCalendarList);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    return { test, res };
   }
 });
 </script>
