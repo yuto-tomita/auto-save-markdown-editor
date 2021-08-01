c7b51a91e9ae0322ff11d9c0ef597336a6b617ae
yuto-tomita <qualidea01@gmail.com>
2021-08-01 15:49:02 +0900

    buttonUIを作成

---
+++ front/src/components/Button/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button
+      class="border border-gray-300 bg-white shadow-sm appearance-none rounded hover:border-blue-300
+			active:bg-gray-300"
+    >
+      <div class="m-1.5 text-gray-600 text-xs">
+        {{ label }}
+      </div>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+	props: {
+		label: {
+			type: String,
+			required: true
+		}
+	}
+});
+</script>
