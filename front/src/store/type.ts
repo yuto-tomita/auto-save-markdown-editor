3447ce1af5612e2b091d2e5f809c4881cf954a45
yuto-tomita <qualidea01@gmail.com>
2021-08-14 19:43:09 +0900

    型定義の命名を修正

--- front/src/store/type.ts
+++ front/src/store/type.ts
@@ -1,10 +1,10 @@
-export interface DraftList {
+export interface Draft {
 	ID: number;
 	Markdown_text?: string
 	Title: string
 }
 export interface ApiState {
-	DraftList: DraftList[]
+	DraftList: Draft[]
 }
 
 export interface DraftParameters {
